fa175868c6d2a41f98534aab79702636e08c6c58
SHLEE <shlee85@lowasis.com>
2025-01-03 15:13:02 +0900

    기본 동작 코드 추가( 재생/일시정지/재생중지 )

--- app/src/main/java/com/example/mediaplayer/MainActivity.kt
+++ app/src/main/java/com/example/mediaplayer/MainActivity.kt
@@ -1,20 +1,120 @@
 package com.example.mediaplayer
 
+import android.content.ComponentName
+import android.content.Intent
+import android.content.ServiceConnection
+import android.os.Build
 import android.os.Bundle
+import android.os.IBinder
+import android.util.Log
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.example.mediaplayer.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    private lateinit var binding: ActivityMainBinding
+    var mService:MusicPlayerService ?= null
+
+    // 서비스와 구성요소 연결 상태 모니터링
+    val mServiceConnection = object : ServiceConnection {
+        override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
+            Log.i(TAG, "onServiceConnected")
+
+            mService = (service as MusicPlayerService.MusicPlayerBinder).getService()
+        }
+
+        override fun onServiceDisconnected(name: ComponentName?) {
+            Log.i(TAG, "onServiceDisconnected")
+
+            mService = null
+        }
+    }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnPlay.setOnClickListener(this)
+        binding.btnPause.setOnClickListener(this)
+        binding.btnStop.setOnClickListener(this)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.i(TAG, "onResume()")
+
+        if(mService == null) {
+            //안드로이드 O이상이면 startForegroundService()를 호출해야 한다.
+            if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                startForegroundService(Intent(
+                    this, MusicPlayerService::class.java
+                ))
+            } else {
+                startService(Intent(
+                    applicationContext, MusicPlayerService::class.java
+                ))
+            }
+
+            //activity를 서비스와 바인드 시킨다.
+            val intent = Intent(this, MusicPlayerService::class.java)
+            bindService(intent, mServiceConnection, BIND_AUTO_CREATE)
         }
     }
+
+    override fun onPause() {
+        super.onPause()
+        Log.i(TAG, "onPause")
+
+        //사용자가 Activity를 떠났을 때
+        if(mService != null) {
+            if(!mService!!.isPlaying()) {   //mService가 재생되고 있지 않다면 서비스를 중단
+                mService!!.stopSelf()
+            }
+            unbindService(mServiceConnection)   //서비스로부터 연결을 끊는다.
+            mService = null
+        }
+    }
+
+    private fun play() {
+        mService?.play()
+    }
+
+    private fun pause() {
+        mService?.pause()
+    }
+
+    private fun stop() {
+        mService?.stop()
+    }
+
+    override fun onClick(v: View?) {
+        when(v?.id) {
+            binding.btnPlay.id -> {
+                Log.i(TAG, "play()")
+                play()
+            }
+
+            binding.btnPause.id -> {
+                Log.i(TAG, "pause()")
+                pause()
+            }
+
+            binding.btnStop.id -> {
+                Log.i(TAG, "stop()")
+                stop()
+            }
+        }
+    }
+
+
+
+    companion object {
+        val TAG = MainActivity::class.java.simpleName
+    }
+
+
 }
---
+++ app/src/main/java/com/example/mediaplayer/MusicPlayerService.kt
@@ -0,0 +1,116 @@
+package com.example.mediaplayer
+
+import android.annotation.SuppressLint
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.Service
+import android.content.Intent
+import android.media.MediaPlayer
+import android.os.Binder
+import android.os.Build
+import android.os.IBinder
+import android.util.Log
+import android.widget.Toast
+
+class MusicPlayerService: Service() {
+    private var mMediaPlayer: MediaPlayer? = null
+    private var mBinder: MusicPlayerBinder? = MusicPlayerBinder()
+
+    override fun onCreate() {
+        super.onCreate()
+        Log.i(TAG, "onCreate()")
+
+        startForegroundService()
+    }
+    override fun onBind(intent: Intent?): IBinder? {
+        Log.i(TAG, "onBind()")
+
+        return mBinder
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        Log.i(TAG, "onStartCommand")
+
+        return START_STICKY //서비스가 중단되면 다시 실행
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.i(TAG, "onDestroy()")
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            stopForeground(true)
+        }
+    }
+
+    private fun startForegroundService() {
+        Log.i(TAG, "startForegroundService()")
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+            val mChannel = NotificationChannel( //알림 채널 설정
+                "CHANNEL_ID","CHANNEL_NAME", NotificationManager.IMPORTANCE_DEFAULT
+            )
+            notificationManager.createNotificationChannel(mChannel)
+        }
+
+        val notification = Notification.Builder(this, "CHANNEL_ID")
+            .setSmallIcon(R.drawable.ic_play)   //알림 아이콘
+            .setContentTitle("뮤직 플레이어 앱")   //알림의 제목 설정
+            .setContentText("앱이 실행 중입니다.")  //아림의 내용 설정
+            .build()
+
+        startForeground(1, notification) //1이상, 알람으로 보여줄 인수 값을 넘겨 준다(notification)
+    }
+
+    fun isPlaying(): Boolean {
+        Log.i(TAG, "isPlaying")
+        return (mMediaPlayer != null && mMediaPlayer?.isPlaying ?: false)
+    }
+
+    fun play() {
+        if(mMediaPlayer == null) {
+            mMediaPlayer = MediaPlayer.create(this, R.raw.chocolate) //음악 파일의 리소스를 가져와 미디어 플레이어 객체를 할당.
+
+            mMediaPlayer?.setVolume(1.0f, 1.0f) //볼륨 저장
+            mMediaPlayer?.isLooping = true //반복재생 여부
+            mMediaPlayer?.start()           //음악재생
+        } else {    //음악 재생 중인 경우
+            if(mMediaPlayer!!.isPlaying) {
+                Toast.makeText(this, "이미 음악이 재생 중입니다.", Toast.LENGTH_SHORT).show()
+            } else {
+                mMediaPlayer?.start()   //음악 재생
+            }
+        }
+    }
+
+    fun pause() {
+        mMediaPlayer?.let {
+            if(it.isPlaying) {
+                it.pause()  //음악을 일시 정지
+            }
+        }
+    }
+
+    fun stop() {
+        mMediaPlayer?.let {
+            if(it.isPlaying) {
+                it.stop()   //음악을 중지
+                it.release()    //미디어 플레이어에 할당된 자원을 해제
+                mMediaPlayer = null
+            }
+        }
+    }
+
+    //바인더를 반환하여 서비스 함수를 사용할 수 있게 한다.
+    inner class MusicPlayerBinder: Binder() {
+        fun getService(): MusicPlayerService {
+            return this@MusicPlayerService
+        }
+    }
+
+    companion object {
+        val TAG = MusicPlayerService::class.java.simpleName
+    }
+}
